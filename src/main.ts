1b8f66265e2250990b2f5bd82bb3be4fa36fea5d
pviniciusm <pvinics@gmail.com>
2022-01-04 20:44:49 -0300

    user feature

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,3 @@
-import express from 'express';
-import cors from 'cors';
 import { DatabaseConnection } from './core/database/connections/connection';
 import 'reflect-metadata';
 import { initServer } from './core/presentation/server';
