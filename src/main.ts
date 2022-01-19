0009f028e04de1dcabe573a484b10f6a4ff41367
pviniciusm <pvinics@gmail.com>
2022-01-19 08:53:19 -0300

    redis first steps

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,25 @@
-import { DatabaseConnection } from './core/infra/database/connections/connection';
-import 'reflect-metadata';
-import { initServer } from './core/presentation/server';
+import { DatabaseConnection } from "./core/infra/database/connections/connection";
+import "reflect-metadata";
+import { initServer } from "./core/presentation/server";
+
+import redis from "ioredis";
 
 DatabaseConnection.initConnection()
     .then(() => {
-        initServer()
+        initServer();
+
+        // Fixme: organizar a conexão e os repositories do redis
+        const connection = new redis();
+        connection.hgetall("usuario2").then((result) => {
+            console.log(result);
+            console.log(result.nome);
+        });
+
+        const data = {
+            nome: "testando",
+            ano: 2022,
+        };
+        connection.set("testenode", JSON.stringify(data));
     })
     .catch((error) => {
         console.log(error);
